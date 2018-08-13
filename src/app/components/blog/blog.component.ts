06eb48192e472925ce12b6fdd27a54777506457a
Maria Perdomo <mperdomo@matisses.co>
2018-08-13 16:49:46 -0500

    prueba
    prueba de subida

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -15,14 +15,14 @@ export class BlogComponent implements OnInit {
 
     constructor(private _route: ActivatedRoute, private _router: Router, private meta: Meta,private title1: Title) {
     this.title1.setTitle('Blog Matisses');
-    this.meta.addTag({ name: 'title', content: 'Blog Matisses' });
+    this.meta.addTag({ name: 'title', content: 'Blog Matisses---' });
     this.meta.addTag({ name: 'keywords', content: 'blog, matisses, decoracion, tips' });
     this.meta.addTag({ name: 'description', content: 'Blog matisses-' });
     this.meta.addTag({ name: 'image', content: 'https://www.matisses.co/assets/images/blog/aparadores.jpg' });
     this.meta.addTag({ property: 'og:url', content: 'https://www.matisses.co/assets/images/blog/aparadores.jpg' });
-    this.meta.addTag({ property: 'og:title', content: 'Blog Matisses' });
+    this.meta.addTag({ property: 'og:title', content: 'Blog Matisses---' });
     this.meta.addTag({ property: 'og:image', content: 'https://www.matisses.co/assets/images/blog/aparadores.jpg' });
-    this.meta.addTag({ property: 'og:description', content: 'Blog Matisses' });
+    this.meta.addTag({ property: 'og:description', content: 'Blog Matisses---' });
     }
 
     ngOnInit() {
@@ -33,9 +33,9 @@ export class BlogComponent implements OnInit {
       $(document).ready(function() {
         $("html, body").animate({scrollTop: 0}, 1000);
       });
-      
+
     }
-  
+
     public seleccionarBlog(id) {
       this.blogNumber = id;
     }

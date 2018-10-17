9306d3718e603216bad5f5fc88ef2245c10a196c
Alejandro <aguerra@matisses.co>
2018-10-17 17:29:48 -0500

    campaña new arrivals

--- src/app/components/principal/slide2/slide-2.component.ts
+++ src/app/components/principal/slide2/slide-2.component.ts
@@ -26,13 +26,13 @@ export class Slide2Component implements OnInit {
 
   private inicializarItems() {
     this.items = new Array<Item>();
-    this._itemService.find('2210021').subscribe(
+    this._itemService.find('1051875').subscribe(
       response => {
         this.items.push(response.result[0]);
-        this._itemService.find('2150002').subscribe(
+        this._itemService.find('1051864').subscribe(
           response => {
             this.items.push(response.result[0]);
-            this._itemService.find('2010053').subscribe(
+            this._itemService.find('1051870').subscribe(
               response => {
                 this.items.push(response.result[0]);
               }, error => { console.error(); }

abcbe8a3f0f1b22093d615d5f56b763fc441d8f3
Alejandro <aguerra@matisses.co>
2018-10-30 17:45:49 -0500

    campaña mesas auxiliares

--- src/app/components/principal/slide1/slide-1.component.ts
+++ src/app/components/principal/slide1/slide-1.component.ts
@@ -26,13 +26,13 @@ export class Slide1Component implements OnInit {
 
   private inicializarItems() {
     this.items = new Array<Item>();
-    this._itemService.find('2260469').subscribe(
+    this._itemService.find('2640027').subscribe(
       response => {
         this.items.push(response.result[0]);
-        this._itemService.find('2290178').subscribe(
+        this._itemService.find('2170038').subscribe(
           response => {
             this.items.push(response.result[0]);
-            this._itemService.find('2290184').subscribe(
+            this._itemService.find('2640017').subscribe(
               response => {
                 this.items.push(response.result[0]);
               }, error => { console.error(); }

e6c0fce45247b9919bf732917d3a47c994367616
Alejandro <aguerra@matisses.co>
2019-01-08 12:03:32 -0500

    REGRESO A TRABAJAR

--- src/app/components/principal/slide2/slide-2.component.ts
+++ src/app/components/principal/slide2/slide-2.component.ts
@@ -26,13 +26,13 @@ export class Slide2Component implements OnInit {
 
   private inicializarItems() {
     this.items = new Array<Item>();
-    this._itemService.find('2090099').subscribe(
+    this._itemService.find('1051845').subscribe(
       response => {
         this.items.push(response.result[0]);
-        this._itemService.find('2130086').subscribe(
+        this._itemService.find('1051846').subscribe(
           response => {
             this.items.push(response.result[0]);
-            this._itemService.find('1051871').subscribe(
+            this._itemService.find('1330478').subscribe(
               response => {
                 this.items.push(response.result[0]);
               }, error => { console.error(); }

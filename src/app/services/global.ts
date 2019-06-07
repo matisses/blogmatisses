ceb75f828a226db39d6a4d2358d1c2571d358f85
Maria Perdomo <mperdomo@matisses.co>
2019-06-07 14:42:35 -0500

    imagenes reemplazo quitar mercado  libre 12 cuotas

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,11 +1,22 @@
+// export let GLOBAL = {
+//   url: 'https://api.matisses.co:8081/api/',
+//   // url: 'http://192.168.5.23:3977/api/',
+//   urlBCS: 'https://360.matisses.co:8443/BCS/res/',
+//   // urlBCS: 'http://192.168.5.155:8080/BCS/res/',
+//   recaptchaSiteKey: '6LekJy0UAAAAANoeggt1-X3VqaROvs97RZI8g3Co',
+//   toMail: 'info@matisses.co',
+//   urlTransactionResult: 'https://www.matisses.co/resultado-transaccion/',
+//   urlTransactionResultList: 'https://www.matisses.co/resultado-transaccion-regalos/',
+//   urlShared: 'https://360.matisses.co:8443/shared/lista-regalos/'
+// };
 export let GLOBAL = {
   url: 'https://api.matisses.co:8081/api/',
   // url: 'http://192.168.5.23:3977/api/',
   urlBCS: 'https://360.matisses.co:8443/BCS/res/',
-  // urlBCS: 'http://192.168.5.155:8080/BCS/res/',
+  //urlBCS: 'http://192.168.5.23:8080/BCS/res/',
   recaptchaSiteKey: '6LekJy0UAAAAANoeggt1-X3VqaROvs97RZI8g3Co',
   toMail: 'info@matisses.co',
-  urlTransactionResult: 'https://www.matisses.co/resultado-transaccion/',
-  urlTransactionResultList: 'https://www.matisses.co/resultado-transaccion-regalos/',
+  urlTransactionResult: 'http://localhost:4200/resultado-transaccion/',
+  urlTransactionResultList: 'http://localhost:4200/resultado-transaccion-regalos/',
   urlShared: 'https://360.matisses.co:8443/shared/lista-regalos/'
 };
